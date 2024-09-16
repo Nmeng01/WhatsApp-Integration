76d94876fba6aaf1e25c19b8696f3950a3fa034a
Nmeng01 <ndm9914@nyu.edu>
2024-09-16 16:58:31 -0400

    update conversation in Zendesk functionality

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import logging
 from dotenv import load_dotenv
 import os
 import asyncio
+from datetime import date
+import time
 
 app = Quart(__name__)
 
@@ -31,6 +33,7 @@ async def webhook():
         load_dotenv()
         subdomain = os.getenv('SUBDOMAIN')
         phone = data['entry'][0]['changes'][0]['value']['messages'][0]['from']
+        # Search for user
         user_url = f'https://{subdomain}.zendesk.com/api/v2/users/search.json?query={phone}'
         async with httpx.AsyncClient() as client:
             response = await client.get(user_url, auth=(os.getenv('Z_EMAIL'), os.getenv('Z_TOKEN')))
@@ -40,6 +43,7 @@ async def webhook():
             if users:
                 requester_id = users[0]['id']
             else:
+                # If user does not exist, create a new user
                 user_url = f'https://{subdomain}.zendesk.com/api/v2/users.json'
                 payload = {
                     "user": {
@@ -57,35 +61,76 @@ async def webhook():
         else:
             logging.error('Failed to search user in Zendesk')
             return jsonify({'error': 'Failed to search user in Zendesk'}), 500
-
-        ticket_url = f'https://{subdomain}.zendesk.com/api/v2/tickets'
-        payload = {
-            "ticket": {
-                "requester_id": requester_id,
-                "subject": f"WhatsApp message from {phone}",
-                "comment": {
-                    "body": f"{data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']}"
-                }
-
-            }
-        }
-
-        async with httpx.AsyncClient() as client:
-            response = await client.post(ticket_url, auth=(os.getenv('Z_EMAIL'), os.getenv('Z_TOKEN')), headers={'Content-Type': 'application/json'}, json=payload)
-        if response.status_code == 201:
-            await asyncio.sleep(10)
-            delete_id = response.json()['ticket']['id'] + 1
-            delete_url = f'https://{subdomain}.zendesk.com/api/v2/tickets/{delete_id}.json'
-            async with httpx.AsyncClient() as client:
-                response = await client.delete(delete_url, auth=(os.getenv('Z_EMAIL'), os.getenv('Z_TOKEN')))
-            if response.status_code == 204:
-                return jsonify({"status": "success", "message": "Message processed successfully"}), 200
-            else:
-                logging.error(f"Failed to delete ticket {delete_id}. Status code: {response.status_code}")
-                return jsonify({'error': 'Failed to delete ticket in Zendesk'}), 500
+        
+        # Check if current ticket exists
+        today = date.today()
+        dst = time.localtime().tm_isdst
+        if dst > 0:
+            start_time = f'{today}T13:00:00Z'
+            end_time = f'{today}T22:00:00Z'
         else:
-            logging.error('Failed to create new ticket in Zendesk')
-            return jsonify({'error': 'Failed to create ticket in Zendesk'}), 500
+            start_time = f'{today}T14:00:00Z'
+            end_time = f'{today}T23:00:00Z'
+
+        search_url = f'https://{subdomain}.zendesk.com/api/v2/search.json'
+        query = f'type:ticket subject:\"{f"WhatsApp message from {phone}"}\" created>{start_time} created<{end_time}'
+        async with httpx.AsyncClient() as client:
+            response = await client.get(search_url, params={"query": query}, auth=(os.getenv('Z_EMAIL'), os.getenv('Z_TOKEN')))
+        if response.status_code == 200:
+            results = response.json().get('results', [])
+            # If current ticket exists, update that ticket, otherwise create a new ticket
+            if results:
+                ticket_id = results[0]['id'] 
+                ticket_url = f'https://{subdomain}.zendesk.com/api/v2/tickets/{ticket_id}.json'
+                payload = {
+                    "ticket": {
+                        "comment": {
+                            "body": f"{data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']}",
+                            "public": True,
+                            "author_id": requester_id
+                        }
+                    }
+                }
+                async with httpx.AsyncClient() as client:
+                    response = await client.put(ticket_url, auth=(os.getenv('Z_EMAIL'), os.getenv('Z_TOKEN')), json=payload)
+                if response.status_code == 200:
+                    return jsonify({"status": "success", "message": "Message processed successfully"}), 200
+                else:
+                    logging.error(f'Failed to update ticket {ticket_id} in Zendesk')
+                    return jsonify({'error': 'Failed to update ticket in Zendesk'}), 500
+            else:
+                ticket_url = f'https://{subdomain}.zendesk.com/api/v2/tickets'
+                payload = {
+                    "ticket": {
+                        "requester_id": requester_id,
+                        "subject": f"WhatsApp message from {phone}",
+                        "comment": {
+                            "body": f"{data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']}",
+                            "public": True
+                        }
+        
+                    }
+                }
+        
+                async with httpx.AsyncClient() as client:
+                    response = await client.post(ticket_url, auth=(os.getenv('Z_EMAIL'), os.getenv('Z_TOKEN')), headers={'Content-Type': 'application/json'}, json=payload)
+                if response.status_code == 201:
+                    await asyncio.sleep(10)
+                    delete_id = response.json()['ticket']['id'] + 1
+                    delete_url = f'https://{subdomain}.zendesk.com/api/v2/tickets/{delete_id}.json'
+                    async with httpx.AsyncClient() as client:
+                        response = await client.delete(delete_url, auth=(os.getenv('Z_EMAIL'), os.getenv('Z_TOKEN')))
+                    if response.status_code == 204:
+                        return jsonify({"status": "success", "message": "Message processed successfully"}), 200
+                    else:
+                        logging.error(f"Failed to delete ticket {delete_id}. Status code: {response.status_code}")
+                        return jsonify({'error': 'Failed to delete ticket in Zendesk'}), 200
+                else:
+                    logging.error('Failed to create new ticket in Zendesk')
+                    return jsonify({'error': 'Failed to create ticket in Zendesk'}), 500
+        else:
+            logging.error(f"Failed to search for existing ticket to update. Status code: {response.status_code}")
+            return jsonify({'error': 'Failed to search for existing ticket to update.'}), 500
         
     except httpx.RequestError as e:
         logging.error(f'Request failed: {str(e)}')
